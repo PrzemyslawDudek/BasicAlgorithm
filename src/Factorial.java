58db4d39e4e9d4786cb6d48149a06037a0b9bace
Przemek <dudek.mail@mail.com>
2019-10-17 23:07:48 +0200

    Must Have

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,3 +1,9 @@
-public class Factorial {
-
+class Factorial {
+    Factorial(){}
+    int getFactorialNumber(int n) {
+        if (n < 0) return -1;
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+        return n * getFactorialNumber(n - 1);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         Fibonacci fibonacci = new Fibonacci();
+        Factorial factorial = new Factorial();
         System.out.println(fibonacci.getFibonacciNumber(-1));
+        System.out.println(factorial.getFactorialNumber(2));
     }
 }
